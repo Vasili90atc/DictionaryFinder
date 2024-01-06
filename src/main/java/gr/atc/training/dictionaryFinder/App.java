c1e24d523dfda6aa8d8a87e1d38f8787f7924aa9
Ioanna-Nikoletta Vasili <IVasili@atc.gr>
2024-01-06 15:58:50 +0200

    new commit

--- src/main/java/gr/atc/training/dictionaryFinder/App.java
+++ src/main/java/gr/atc/training/dictionaryFinder/App.java
@@ -1,42 +1,39 @@
 package gr.atc.training.dictionaryFinder;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-		Dictionary dict = new Dictionary();
+public class App {
+	public static void main(String[] args) {
+		Dictionary dict = new Dictionary("english3.txt");
+		
+		/*
 		int count = dict.getWordCount(null, "ab", 0);
 		count = dict.getWordCount(null, "is", 0);
-		
-		
-		 count = dict.getWordCount("ab", null, 5);
-		 count = dict.getWordCount("bc", null, 5);
-		
-		 count = dict.getWordCount(null, "qw", 7);
-		 count = dict.getWordCount(null, "er", 7);
-			
-		 count = dict.getWordCount("ko", "yu", 3);
-		 count = dict.getWordCount("ge", "io", 3);
-				
+		count = dict.getWordCount("ab", null, 5);
+		count = dict.getWordCount("bc", null, 5);
+		count = dict.getWordCount(null, "qw", 7);
+		count = dict.getWordCount(null, "er", 7);
+		count = dict.getWordCount("ko", "yu", 3);
+		count = dict.getWordCount("ge", "io", 3);
 		count = dict.getWordCount(null, "sd", 9);
 		count = dict.getWordCount(null, "fd", 9);
-					
 		count = dict.getWordCount("ab", "gh", 0);
 		count = dict.getWordCount("is", "jk", 0);
-						
 		count = dict.getWordCount(null, "kl", 8);
 		count = dict.getWordCount(null, "zx", 8);
-							
 		count = dict.getWordCount("is", "cv", 6);
 		count = dict.getWordCount("ab", "bn", 6);
-								
+
 		count = dict.getWordCount("ma", null, 10);
 		count = dict.getWordCount("nd", null, 10);
-									
+
 		count = dict.getWordCount("re", "iu", 0);
 		count = dict.getWordCount("pu", "rd", 0);
-		
+
 		System.out.println("Count: " + count);
-	
-}
+		System.out.println("CountWord " + countWord);
+		
+		*/
+		
+		//dict.getSpellingCheck("text.txt");
+		dict.print_N_random_words(7);
+	}
 }
--- src/main/java/gr/atc/training/dictionaryFinder/Dictionary.java
+++ src/main/java/gr/atc/training/dictionaryFinder/Dictionary.java
@@ -5,11 +5,18 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
 
 public class Dictionary {
-	ArrayList<String> words = new ArrayList<String>();
+	ArrayList<String> dictionary_words = new ArrayList<String>();
 	
-	private void readFileToList(String path) {
+	public Dictionary(String dict_path) {
+		dictionary_words = readFileToList(dict_path);
+	}
+
+	private ArrayList<String> readFileToList(String path) {
+		ArrayList<String> words = new ArrayList<String>();
 		String word = "";
 		BufferedReader reader = null;
 		try {
@@ -22,16 +29,13 @@ public class Dictionary {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		return words;
 	}
-	
+
 	public int getWordCount(String prefix, String suffix, int letterCount) {
-		
-		// Get words from file
-		readFileToList("english3.txt");
-		
 		// Count how many words in words arraylist are valid
 		int count = 0;
-		for (String w:words) {
+		for (String w : dictionary_words) {
 			if (prefix != null && w.startsWith(prefix) == false) {
 				continue;
 			}
@@ -45,6 +49,41 @@ public class Dictionary {
 			count++;
 		}
 		return count;
+	}
+
+	public void getSpellingCheck(String text_path) {
+		// Read words from text file
+		//ArrayList<String> text_words = new ArrayList<String>();
+		String line = "";
+		BufferedReader reader = null;
+		try {
+			reader = new BufferedReader(new InputStreamReader(new FileInputStream(new File(text_path)), "UTF-8"));
+			line = reader.readLine();
+			System.out.println("Words in text not contained in dictionary:");
+			while (line != null) {
+				String[] words = line.replaceAll("[^a-zA-Z ]", "").toLowerCase().split(" ");
+				//text_words.addAll(Arrays.asList(words));
+				for (String w : words) {
+					if (!w.equals("") && !dictionary_words.contains(w)) {
+						System.out.print(w + " ");
+					}
+				}
+				line = reader.readLine();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return;
+	}
+	
+	public void print_N_random_words(int N) {
+		Random rand = new Random();
+		System.out.println("Printing " + N + " random words from the dictionary:");
+		for (int i=0; i<N; i++) {
+			int random_word_idx = rand.nextInt(dictionary_words.size()-1);
+			System.out.print(dictionary_words.get(random_word_idx) + " ");
+		}
+		System.out.println();
 	}
 
 }
